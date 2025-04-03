7b0545275c0e5adeffd41c4ca665b6ba593d5393
Mikołaj Linczewski <mlinczewski@deloittece.com>
2025-04-03 17:03:55 +0200

    homepage

--- index.js
+++ index.js
@@ -8,3 +8,15 @@ app.use(express.static("public"));
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+app.get("/", (req, res) => {
+    res.render("homepage.ejs")
+});
+
+app.get("/reservations", (req, res) => {
+    res.render("reservations.ejs")
+});
+
+app.get("/menu", (req, res) => {
+    res.render("menu.ejs")
+});
